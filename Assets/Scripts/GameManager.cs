9e1b26eb60f3cd8db4c59846d43e2e87a81afc45
Gal Afik <gal.p.afik@gmail.com>
2020-02-27 15:47:59 -0500

    Added UI elements

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,29 +7,55 @@ namespace AndroidDash
 {
 	public class GameManager : MonoBehaviour, IObserver
 	{
-		private GameObject Player;
-		private GameObject EnemyManager;
-		private int Score = 0; // How many points have been earned
+		[Serializable]
+		public class ConfigurationData
+		{
+			public GameObject PlayerObject;
+			public GameObject EnemyManagerObject;
+			public GameObject UICanvasObject;
+		}
+		[Serializable]
+		private class StateData
+		{
+			public Player Player;
+			public EnemyManager EnemyManager;
+			public GameUserInterface UICanvas;
+			public int Score = 0; // How many points have been earned
+		}
+		public ConfigurationData Conf = new ConfigurationData();
+		private StateData State = new StateData();
+
 
 		void Start()
 		{
-			// Get the player and bubbles manager objects
-			Player = GameObject.FindWithTag("Player");
-			EnemyManager = GameObject.FindWithTag("EnemyManager");
+			// Get private class references for all public objects
+			State.Player = Conf.PlayerObject.GetComponent<Player>();
+			State.EnemyManager = Conf.EnemyManagerObject.GetComponent<EnemyManager>();
+			State.UICanvas = Conf.UICanvasObject.GetComponent<GameUserInterface>();
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
 			// If the player has no more health
-			if (Player.GetComponent<Player>().CurrentState == Player.GetComponent<Player>().Dead)
+			if (State.Player.CurrentState == State.Player.Dead)
 			{
 				// Stop all bubbles from chasing the player
-				EnemyManager.GetComponent<EnemyManager>().StopAllEnemies();
+				State.EnemyManager.StopAllEnemies();
 
 				// TODO : end the game
 				
 			}
+		}
+
+		// Runs when the GUI is updated
+		private void OnGUI()
+		{
+			// Update the score UI text
+			State.UICanvas.UpdateScore(State.Score);
+
+			// Update the player health UI
+			State.UICanvas.UpdateHearts(State.Player.Idle.GetHealth());
 		}
 
 		public void OnNotify(GameEvent e, int value)
@@ -39,8 +65,8 @@ namespace AndroidDash
 				case GameEvent.PLAYER_ATTACK:
 				{
 					print("GameManager::OnNotify.PLAYER_ATTACK");
-					Score += value;
-						print("GameManager::Score = " + Score);
+					// Update the score
+					State.Score += value;
 					break;
 				}
 				case GameEvent.PLAYER_DASH:
